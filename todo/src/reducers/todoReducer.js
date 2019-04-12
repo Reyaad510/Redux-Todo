c2cb46e0d8e0b39d8420471dc170b5622e4ce1ad
Reyaad Rafeek <reyaad510@gmail.com>
2019-04-12 16:52:25 -0400

    Completed todoReducer and adding a todo functionality

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,5 +1,5 @@
 
-
+import { ADD_TODO } from '../actions';
 
 const initialState = {
     todos: [
@@ -13,6 +13,16 @@ const initialState = {
 
 export const todoReducer = (state = initialState, action) => {
     switch(action.type) {
+        case ADD_TODO:
+        console.log(action); 
+        const newTodo = {
+            todo: action.payload,
+            completed: false
+        };
+        return {
+            ...state,
+            todos: [...state.todos, newTodo]
+        };
         default:
         return state;
     }
