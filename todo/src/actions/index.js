23049968843043769156e76b59308fad6c29ebaf
Reyaad Rafeek <reyaad510@gmail.com>
2019-04-13 11:44:16 -0400

    Created toggleTodo action creator

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,9 +1,18 @@
 export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO='TOGGLE_TODO';
 
 export function addNewTodo(newTodo) {
     console.log(newTodo)
     return {
         type: ADD_TODO,
         payload: newTodo
+    };
+}
+
+export function toggleTodo(index) {
+    console.log(index);
+    return {
+        type: TOGGLE_TODO,
+        payload: index
     };
 }
